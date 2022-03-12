f1708856627b3fe2ed55000e38ebeca863ed0e94
Eden Kitaw <edenkitaw19@gmail.com>
2022-03-12 11:34:49 +0300

    commited

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,10 +1,15 @@
 #include<stdio.h>
+/**
+ * main - eden
+ * Return : Always 0
+ *
+ */
 int main(void)
 {
 	char A;
-	for (A = 'a' ; A <= 'Z' ; A++)
-
+	for (A = 'a'; A <= 'Z'; A++)
 		putchar(A);
+	putchar('\n');
 	return (0);
 
 }
