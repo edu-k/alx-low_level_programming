e6af4b3e633e68029298b71e6ce7f052030abd3d
Eden Kitaw <edenkitaw19@gmail.com>
2022-03-12 18:04:18 +0300

    commited

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,11 @@
 #include <stdio.h>
-<<<<<<< HEAD
-/*
+/**
  *main - print
+ *Return: 0
  *
- *Return- 0
  */
 int main(void)
 {
-	    puts("\"Programming is like building a multilingual puzzle");
-	        return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-=======
-
-int main(void)
-{
-    puts("Programming is like building a multilingual puzzle");
-    return 0;
-}
-
->>>>>>> 6ebb67ede432ae5b6447dd82da3dbfdf043fc6fc
