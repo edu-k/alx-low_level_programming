4d4cf87a2d54762521bac1da822e36c2fd2a8f85
Eden Kitaw <edenkitaw19@gmail.com>
2022-03-16 01:30:14 +0300

    commited

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,19 @@
-#include<stdio.h>
 #include "main.h"
 /**
- * main - Lanzador
- * Return: Always 0
+ * main - Prints Holberton
  *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	_putchar("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
