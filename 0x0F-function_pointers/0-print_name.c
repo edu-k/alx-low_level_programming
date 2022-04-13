b7ddab1b62e5037ed32b5a82c38af06daee1730b
Eden Kitaw <edenkitaw19@gmail.com>
2022-04-13 22:54:18 +0300

    commited

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,6 @@
 #include "function_pointers.h"
 
-/*
+/**
  * print_name - Prints a name.
  * @name: The name to be printed.
  * @f: A pointer to a function that prints a name.
--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -8,11 +8,12 @@
  *       prototypes used by the 3-main.c program.
  */
 
-/*
+/**
  * struct op - A struct op.
  * @op: The operator.
  * @f: The associated function.
  */
+
 typedef struct op
 {
 	char *op;
