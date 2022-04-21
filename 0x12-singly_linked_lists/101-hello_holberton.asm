36a8a9074631d31951c305a7a383294e31d12341
Eden Kitaw <edenkitaw19@gmail.com>
2022-04-21 22:50:09 +0300

    commited

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,14 @@
+global _start
+section .data
+hello:
+printf"Hello, Holberton", 14
+section .text
+_start:
+mov rax, 1
+mov rdi, 1
+mov rsi, hello
+mov rdx, 13
+syscall
+mov rax, 60
+xor rdi, rdi
+syscall
